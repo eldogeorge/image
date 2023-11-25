f555797e2820b8d8a9a6f9fcbeaf413abd61da86
unknown <eldogeorgenettadiyil@gmail.com>
2023-11-25 21:05:41 +0530

    completed

--- erpclient/src/Services/BaseUrl.js
+++ erpclient/src/Services/BaseUrl.js
@@ -1,5 +1,6 @@
 
 // AAS1 and after exports
-const baseURL='http://localhost:8000'
+// const baseURL='http://localhost:8000'
+const baseURL='https://image-server-6tzw.onrender.com'
 
 module.exports=baseURL
